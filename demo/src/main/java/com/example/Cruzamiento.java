dcf686db501529fdc290aa100a7d966116213f59
mael098 <juanvillegasmorales@outlook.com>
2026-05-11 09:54:46 -0600

    Implement crossover functionality in Cruzamiento class and update Main class to display crossover between best and random individuals

--- demo/src/main/java/com/example/Cruzamiento.java
+++ demo/src/main/java/com/example/Cruzamiento.java
@@ -1,5 +1,33 @@
 package com.example;
 
 public class Cruzamiento {
-    
+    Individuo apto2;Individuo apto1;
+
+    public Cruzamiento(Individuo apto1, Individuo apto2) {
+        this.apto1 = apto1;
+        this.apto2 = apto2;
+    }
+
+    public Individuo cruzar() {
+
+        int dimensiones  = apto1.genes.length;
+        Individuo hijo = new Individuo(dimensiones, 0, 1);
+        for (int i = 0; i < dimensiones; i++) {
+            // Elegir aleatoriamente el gen del padre 1 o del padre 2
+            if (Math.random() > 0.5) {
+                hijo.genes[i] = apto1.genes[i];
+            } else {
+                hijo.genes[i] = apto2.genes[i];
+            }
+
+        }
+        return hijo;
+    }
+
+
+
+
+
+
+
 }
--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -16,5 +16,10 @@ public class Main {
         Individuo mejor = poblacion.getMejor();
         System.out.println(mejor);
 
+        System.out.println("Cruzamiento entre el mejor individuo y otro individuo aleatorio:");
+        Individuo otro = poblacion.individuos.get((int)(Math.random() * tamañoPoblacion));
+        System.out.println("Otro individuo:");
+        System.out.println(otro);
+
     }
 }
